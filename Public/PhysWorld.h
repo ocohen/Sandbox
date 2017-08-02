3b38f6e4f361127dffb4188244fb0960d0d8dbde
Ori Cohen <oricohen139@gmail.com>
2017-08-01 23:07:53 -0400

    Added barebone rigid body and physics integrate/solve loop

--- Public/PhysWorld.h
+++ Public/PhysWorld.h
@@ -3,14 +3,76 @@
 
 #include <vector>
 #include "RigidBody.h"
+#include "Renderer.h"
 
 class PhysWorld
 {
 public:
-	void Simulate(float DeltaTime);
+    PhysWorld(const Vector3& inGravity)
+        : gravity(inGravity)
+    {
+    }
+
+	void simulate(float deltaTime)
+    {
+        //integrate unbounded velocities
+        for(RigidBody& rb : bodies)
+        {
+            if(rb.invMass > 0.f)
+            {
+                rb.linearVelocity += gravity * deltaTime;
+            }
+        }
+
+        //update constraints
+
+        //solve constraints
+
+        //integrate position
+        for (RigidBody& rb : bodies)
+        {
+            if (rb.invMass > 0.f)
+            {
+                rb.bodyToWorld.translation += rb.linearVelocity * deltaTime;
+                //todo: angular velocity
+            }
+        }
+    }
+
+    int createRigidBody(const Transform& bodyToWorld, const RigidBodyDesc& rigidBodyDesc)
+    {
+        const int ret = (int)bodies.size();
+        bodies.emplace_back(bodyToWorld, rigidBodyDesc);
+        return ret;
+    }
 
 private:
 	std::vector<RigidBody> bodies;
+    Vector3 gravity;
+    friend class PhysWorldDebugger;
+};
+
+class PhysWorldDebugger
+{
+public:
+    PhysWorldDebugger(const PhysWorld& inWorld, Renderer& inRenderer)
+        : world(inWorld)
+        , renderer(inRenderer)
+    {
+    }
+
+    void debugDraw() const
+    {
+        for(const RigidBody& rigidBody : world.bodies)
+        {
+            renderer.drawOrientedCircles(rigidBody.bodyToWorld, 3.f, 16, 2.f);
+            renderer.drawCross(rigidBody.bodyToWorld, 3.f, 2.f);
+        }
+    }
+
+private:
+    const PhysWorld& world;
+    Renderer& renderer;
 };
 
 #endif
--- Public/RigidBody.h
+++ Public/RigidBody.h
@@ -4,10 +4,44 @@
 #include "Vector3.h"
 #include "Transform.h"
 
+struct RigidBodyDesc
+{
+    RigidBodyDesc()
+        : invInertia(1.f, 1.f, 1.f)
+        , invMass(1.f)
+        , linearDamping(0.f)
+        , angularDamping(0.f)
+    {
+    }
+
+    Vector3 invInertia;
+    float invMass;
+    float linearDamping;
+    float angularDamping;
+    //geometry info
+};
+
 struct RigidBody
 {
+    RigidBody()
+    {
+    }
+
+    RigidBody(const Transform& inBodyToWorld, const RigidBodyDesc& rigidBodyDesc)
+        : invInertia(rigidBodyDesc.invInertia)
+        , invMass(rigidBodyDesc.invMass)
+        , linearDamping(rigidBodyDesc.linearDamping)
+        , angularDamping(rigidBodyDesc.angularDamping)
+        , linearVelocity(0.f, 0.f, 0.f)
+        , angularVelocity(0.f, 0.f, 0.f)
+        , bodyToWorld(inBodyToWorld)
+    {
+    }
+
+    Vector3 invInertia;
 	float invMass;
-	Vector3 invInertia;
+    float linearDamping;
+    float angularDamping;
 
 	Vector3 linearVelocity;
 	Vector3 angularVelocity;
